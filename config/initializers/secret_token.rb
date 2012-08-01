65e9c8ce158426d9a3eee42663ad573535d407e5
Jorge Pardinas <jparbros@Jorges-MacBook-Pro.local>
2012-07-31 20:51:12 -0500

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PpPeru::Application.config.secret_token = '39b4856983366943a0d95b3e864aea6d213cd8fa1c89d9663139e54ec355585b81406cf9eb726e65e117e5e15fd6b91673add97b39d79e81731c30eebfbbee5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PpPeru::Application.config.session_store :cookie_store, key: '_pp_peru_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PpPeru::Application.config.session_store :active_record_store
