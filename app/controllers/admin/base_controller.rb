fe865436bd765d78aa5b4a385d399acbdea3fa61
David Madrigal <mc.david.madrigal@gmail.com>
2012-08-11 14:14:00 -0500

    only admin user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,10 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
+  before_filter :authenticate_user!
+  before_filter :only_admin_user!
+  private
+  
+  def only_admin_user!
+    redirect_to root_path unless current_subject.admin?
+  end
 end
