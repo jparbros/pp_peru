a420beced2aa1dc81c2c7bca65aa85a4f8f036a1
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-09-18 15:00:17 -0500

    users admin crud

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,4 +11,39 @@ class Admin::UsersController < Admin::BaseController
       format.json{ render json: @users.map(&:attributes) }
     end
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    generated_password = Devise.friendly_token.first(6)
+    @user = User.new(params[:user])
+    @user.password = generated_password
+    @user.password_confirmation = generated_password
+    if @user.save
+      redirect_to admin_users_path, notice: 'Creado Correctamente'
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @user = User.find params[:id]
+  end
+  
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(params[:user])
+      redirect_to admin_users_path, notice: 'Actualizado Correctamente'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find params[:id]
+    @user.destroy
+    redirect_to admin_users_path, notice: 'Eliminado Correctamente'
+  end
 end
