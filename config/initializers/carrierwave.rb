a6b6d38986ddc58eba1aa7eba459921fb051a709
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-10-31 05:01:32 -0600

    AWS configuraciones

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,11 @@
 CarrierWave.configure do |config|
-  config.storage Rails.env.production? ? :fog : :file 
+  config.storage = (Rails.env.production? ? :fog : :file)
   
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key  =>  ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  config.fog_directory = 'pp_pery'
+  }
+  config.fog_directory = ENV['AWS_BUCKET']
+  config.fog_public    = true
 end
