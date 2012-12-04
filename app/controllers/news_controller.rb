3460741969c582b46a131b5abd7b853bcaab7ee2
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-12-04 12:38:58 -0600

    orden de las noticias created_at desc

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
   respond_to :json, :html
   
   def index
-    @news = News.text_search(params[:query]).published.by_permissions(current_user).by_topics(params[:topic_id])
+    @news = News.text_search(params[:query]).published.by_permissions(current_user).by_topics(params[:topic_id]).order('created_at desc')
   end
 
   def show
