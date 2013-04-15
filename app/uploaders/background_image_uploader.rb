613d8eb376b841e1c93309e4b367975d5218d0ad
David Madrigal <mc.david.madrigal@gmail.com>
2013-04-15 12:29:54 -0500

    add cache dir

--- app/uploaders/background_image_uploader.rb
+++ app/uploaders/background_image_uploader.rb
@@ -24,6 +24,10 @@ class BackgroundImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
+  def cache_dir
+     "#{Rails.root}/tmp/uploads"
+  end
+  
   process :crop
   
   
