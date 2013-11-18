8967706466a8a2125c01629b75424fb859e7b75e
Jorge Pardiñas <jparbros@gmail.com>
2013-11-18 01:04:29 -0600

    mejorsndo asdmin

--- lib/cms_devise_auth.rb
+++ lib/cms_devise_auth.rb
@@ -1,5 +1,5 @@
 module CmsDeviseAuth
   def authenticate
-    redirect_to root_path unless current_user.admin_access?
+    redirect_to root_path unless current_user || current_user.admin_access?
   end
 end
