7ca87669b2ad614def270d53b65a269231bba520
Jorge Pardiñas <jparbros@gmail.com>
2014-02-13 15:46:34 -0600

    email configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ PpPeru::Application.configure do
   # config.threadsafe!
   
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.mandrillapp.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_PASSWORD'],
